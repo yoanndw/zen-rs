db1300e87acd42b6ddb238c09b86d00a0d8e0c97
yoannd <yoanndewilde@gmail.com>
2020-07-09 11:33:45 +0200

    [CLI] Print options in title menu

--- src/cli/menu/title.rs
+++ src/cli/menu/title.rs
@@ -4,7 +4,11 @@ pub struct TitleMenu;
 
 impl Menu for TitleMenu {
     fn update(&mut self) -> MenuTrans {
-        println!("Title menu");
+        println!("-------[ZEN]-------");
+        println!("[1] Nouvelle partie");
+        println!("[2] Reprendre partie");
+        println!("[3] Quitter");
+
         MenuTrans::None
     }
 }
