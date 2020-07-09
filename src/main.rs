fa713062f2f5312543b5faa38d3e8aa2b581ea74
yoannd <yoanndewilde@gmail.com>
2020-07-09 11:10:09 +0200

    Basic app struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 mod cli;
 
+use cli::menu::Menu;
+
+struct Zen {
+    curr_menu: dyn Menu,
+}
+
+impl Zen {
+    fn run(&mut self) {
+        self.curr_menu.update();
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
