9816bd8765c1df0f454a409598144e738f774230
yoannd <yoanndewilde@gmail.com>
2020-07-11 18:38:10 +0200

    [CLI] Print squares + unit tests

--- src/board.rs
+++ src/board.rs
@@ -1,6 +1,21 @@
+use std::fmt::{Display, Formatter, Result as FmtResult};
+
 pub enum Team {
     Black,
     White,
+}
+
+impl Display for Team {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(
+            f,
+            "{}",
+            match self {
+                Team::Black => "b".to_owned(),
+                Team::White => "w".to_owned(),
+            }
+        )
+    }
 }
 
 pub enum Pawn {
@@ -8,6 +23,85 @@ pub enum Pawn {
     Zen,
 }
 
+impl Display for Pawn {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(
+            f,
+            "{}",
+            match self {
+                Pawn::Player(t) => format!("{}", t),
+                Pawn::Zen => "z".to_owned(),
+            }
+        )
+    }
+}
+
 pub struct Square {
     pawn: Option<Pawn>,
+}
+
+impl Display for Square {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(
+            f,
+            "{}",
+            match &self.pawn {
+                Some(p) => format!("{}", p),
+                None => ".".to_owned(),
+            }
+        )
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn print_team() {
+        assert_eq!(format!("{}", Team::Black), "b".to_owned());
+        assert_eq!(format!("{}", Team::White), "w".to_owned());
+    }
+
+    #[test]
+    fn print_pawn() {
+        assert_eq!(format!("{}", Pawn::Player(Team::Black)), "b".to_owned());
+        assert_eq!(format!("{}", Pawn::Player(Team::White)), "w".to_owned());
+        assert_eq!(format!("{}", Pawn::Zen), "z".to_owned());
+    }
+
+    #[test]
+    fn print_square() {
+        assert_eq!(
+            format!(
+                "{}",
+                Square {
+                    pawn: Some(Pawn::Player(Team::Black))
+                }
+            ),
+            "b".to_owned()
+        );
+
+        assert_eq!(
+            format!(
+                "{}",
+                Square {
+                    pawn: Some(Pawn::Player(Team::White))
+                }
+            ),
+            "w".to_owned()
+        );
+
+        assert_eq!(
+            format!(
+                "{}",
+                Square {
+                    pawn: Some(Pawn::Zen)
+                }
+            ),
+            "z".to_owned()
+        );
+
+        assert_eq!(format!("{}", Square { pawn: None }), ".".to_owned());
+    }
 }
