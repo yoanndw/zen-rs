9e5abee68163ace1a454ef60c9a846cf0d85841a
yoannd <yoanndewilde@gmail.com>
2020-07-11 17:09:52 +0200

    [CLI/Input] Use functions instead of struct

--- src/cli/input.rs
+++ src/cli/input.rs
@@ -1,39 +1,29 @@
 use std::io::{self, Stdin, Write};
 
-pub struct Input {
-    stdin: Stdin,
+pub fn read_str(prompt: &str) -> String {
+    print!("\n> {}", prompt);
+    io::stdout().flush();
+
+    let mut s = String::new();
+    io::stdin().read_line(&mut s);
+
+    s
 }
 
-impl Input {
-    pub fn new(stdin: Stdin) -> Self {
-        Self { stdin }
+pub fn read_int(prompt: &str) -> i32 {
+    let mut s = read_str(prompt);
+    while s.parse::<i32>().is_err() {
+        s = read_str(prompt);
     }
 
-    pub fn read_str(&mut self, prompt: &str) -> String {
-        print!("\n> {}", prompt);
-        io::stdout().flush();
+    s.parse().unwrap()
+}
 
-        let mut s = String::new();
-        self.stdin.read_line(&mut s);
-
-        s
+pub fn read_int_ranged(prompt: &str, min: i32, max: i32) -> i32 {
+    let mut i = read_int(prompt);
+    while i < min || i > max {
+        i = read_int(prompt);
     }
 
-    pub fn read_int(&mut self, prompt: &str) -> i32 {
-        let mut s = self.read_str(prompt);
-        while s.parse::<i32>().is_err() {
-            s = self.read_str(prompt);
-        }
-
-        s.parse().unwrap()
-    }
-
-    pub fn read_int_ranged(&mut self, prompt: &str, min: i32, max: i32) -> i32 {
-        let mut i = self.read_int(prompt);
-        while i < min || i > max {
-            i = self.read_int(prompt);
-        }
-
-        i
-    }
+    i
 }
