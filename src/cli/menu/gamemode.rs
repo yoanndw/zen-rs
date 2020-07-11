93a8246723f1db229e7bc73ca2e69e960cd86ea1
yoannd <yoanndewilde@gmail.com>
2020-07-11 17:29:20 +0200

    [CLI] First implementation of gamemode choice menu

--- src/cli/menu/gamemode.rs
+++ src/cli/menu/gamemode.rs
@@ -6,7 +6,14 @@ pub struct GamemodeMenu;
 impl Menu for GamemodeMenu {
     fn update(&mut self) -> MenuTrans {
         println!("-------[NOUVELLE PARTIE]-------");
+        println!("[1] Mode solo");
+        println!("[2] Mode multi");
+        println!("[3] Retour");
 
-        MenuTrans::None
+        let command = input::read_int_ranged("Que voulez-vous faire ?", 1, 3);
+        match command {
+            1 | 2 => MenuTrans::Quit,
+            _ => MenuTrans::Change(MenuKind::Title),
+        }
     }
 }
